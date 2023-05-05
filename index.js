861f0d00f6d5619ccd61e8bb7e4d554aa1e0958b
Manuel Nalerio <manu.nalerio@gmail.com>
2023-05-04 22:29:05 -0300

    Portfolio

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ import { annotate } from "https://unpkg.com/rough-notation?module";
 const tech = document.querySelector('.tech');
 const saludo = document.querySelector('.texto-saludo');
 
-const annotationSaludo = annotate(saludo, { type: 'underline' , color:'yellow'});
-const annotation = annotate(tech, { type: 'underline' , color:'yellow'});
+const annotationSaludo = annotate(saludo, { 
+  type: 'underline',
+  color: 'yellow',
+ 
+});
+
+const annotation = annotate(tech, {
+  type: 'underline',
+  color: 'yellow',
+  animationDuration: 6000 // Duración de la animación en milisegundos
+});
 
 annotation.show();
 annotationSaludo.show();
